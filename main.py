34905c99ddadc74d35a2deb3f866721d5518000e
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-05-30 21:35:16 +0600

    solid_again

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
-# class Figure:
-#     def get_arena(self):
-#         return 0
+class Figure:
+    def get_arena(self):
+        return 0
     
-# class  Square(Shape):
-#     def __init__(self, side):
-#         self.side = side
+class  Square(Shape):
+    def __init__(self, side):
+        self.side = side
 
-#         def get_area(self):
-#             return self.side **2
+        def get_area(self):
+            return self.side **2
         
-# class Rectangle(Figure):
-#     def __init__(self, width, height):
-#         self.width = width
-#         self.height = height
-#     def get_area(self):
-#         return self.width * self.height
+class Rectangle(Figure):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    def get_area(self):
+        return self.width * self.height
 
-# from abc import ABC, abstractmethod
-# class Figure (ABC):
-#     def get_area (self):
-#         pass
-# class Square(Figure):
-#     def __init__ (self, side):
-#         self.side = side
-#     def area (self):
-#         return self.side **2
+from abc import ABC, abstractmethod
+class Figure (ABC):
+    def get_area (self):
+        pass
+class Square(Figure):
+    def __init__ (self, side):
+        self.side = side
+    def area (self):
+        return self.side **2
     
-# class Rectangle(Figure):
-#     def __init__(self, width, height):
-#         self. width = width
-#         self.height = height
-#     def get_area(self):
-#         return self. width * self.height
+class Rectangle(Figure):
+    def __init__(self, width, height):
+        self. width = width
+        self.height = height
+    def get_area(self):
+        return self. width * self.height
 
 
 # Solid- I
@@ -51,3 +51,5 @@ class FirstDocumentEditor(OpenDocument, CloseDocument):
 class SecondDocumnetEditor:
     def save_document(self):
     def close_document(self):
+
+
---
+++ main2.py
@@ -0,0 +1,31 @@
+class Order:
+    items = []
+    quantities = []
+    prices = []
+    status = "open"
+    def add_item(self, items, quantity, price):
+        self.items.append(items)
+        self.quantities.append(quantity) 
+        self.prices.append(price)
+    def total_price(self):
+        total = 0
+        for i in range(len(self.prices)):
+            total += self.quantities [i] * self.prices[i]
+            return total
+    def pay(self, payment_type, security_code):
+        if payment_type == "debit":
+            print("Processing debit type")
+            print(f"Verifying security_code: {security_code}")
+            self.status = "paid"
+        elif payment_type =="credit":
+            print("processing credit card")
+            print(f"Verifying security_code: {security_code}")
+            self.status = "paid"
+        else:
+            raise Exception(f"Unknown payment type: {payment_type}")
+
+
+
+         
+
+
