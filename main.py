0b594504514562a5b1b7d440dc5cc0650b0f889c
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-05-30 21:06:39 +0600

    separation

--- main.py
+++ main.py
@@ -16,19 +16,38 @@
 #     def get_area(self):
 #         return self.width * self.height
 
-from abc import ABC, abstractmethod
-class Figure (ABC):
-    def get_area (self):
-        pass
-class Square(Figure):
-    def __init__ (self, side):
-        self.side = side
-    def area (self):
-        return self.side **2
+# from abc import ABC, abstractmethod
+# class Figure (ABC):
+#     def get_area (self):
+#         pass
+# class Square(Figure):
+#     def __init__ (self, side):
+#         self.side = side
+#     def area (self):
+#         return self.side **2
     
-class Rectangle(Figure):
-    def __init__(self, width, height):
-        self. width = width
-        self.height = height
-    def get_area(self):
-        return self. width * self.height
+# class Rectangle(Figure):
+#     def __init__(self, width, height):
+#         self. width = width
+#         self.height = height
+#     def get_area(self):
+#         return self. width * self.height
+
+
+# Solid- I
+class OpenDocument:
+    def open_document(self):
+        pass
+class SaveDocument:
+    def save_document(self):
+        pass
+class CloseDocument:
+    def close_document(self):
+
+
+class FirstDocumentEditor(OpenDocument, CloseDocument):
+     def open_document(self):
+      def close_document(self):
+class SecondDocumnetEditor:
+    def save_document(self):
+    def close_document(self):
