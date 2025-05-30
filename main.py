09d9a02144e29e04812a0b376cffa93939e22c4f
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-05-30 20:56:27 +0600

    solid-l2

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-class Figure:
-    def get_arena(self):
-        return 0
+# class Figure:
+#     def get_arena(self):
+#         return 0
     
-class  Square(Shape):
-    def __init__(self, side):
-        self.side = side
+# class  Square(Shape):
+#     def __init__(self, side):
+#         self.side = side
 
-        def get_area(self):
-            return self.side **2
+#         def get_area(self):
+#             return self.side **2
         
+# class Rectangle(Figure):
+#     def __init__(self, width, height):
+#         self.width = width
+#         self.height = height
+#     def get_area(self):
+#         return self.width * self.height
+
+from abc import ABC, abstractmethod
+class Figure (ABC):
+    def get_area (self):
+        pass
+class Square(Figure):
+    def __init__ (self, side):
+        self.side = side
+    def area (self):
+        return self.side **2
+    
 class Rectangle(Figure):
     def __init__(self, width, height):
-        self.width = width
+        self. width = width
         self.height = height
     def get_area(self):
-        return self.width * self.height
+        return self. width * self.height
