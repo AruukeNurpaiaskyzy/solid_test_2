6ada2f4ee5e51cf540343ca00b61fb87659ee295
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-05-31 21:18:33 +0600

    solid_OC_finished

--- main2.py
+++ main2.py
@@ -18,19 +18,25 @@ class PaymentProcessor(ABC):
     def pay(self, order, secutiy_code):
         pass
 class DebitPaymentProcessor(PaymentProcessor):
-    def pay(self, order, security_code):
+    def __init__(self, security_code):
+        self.security_code = security_code
+    def pay(self, order):
         print("Processing debit payment type")
-        print(f"Verifying security code: {security_code}")
+        print(f"Verifying security code: {self.security_code}")
         order.status = "paid"
 class CreditPaymentProcessor(PaymentProcessor):
-    def pay(self, order, security_code):
+    def __init__(self, security_code):
+        self.security_code = security_code
+    def pay(self, order ):
         print("Processing credit payment type")
-        print(f"Verifying security code: {security_code}")
+        print(f"Verifying security code: {self.security_code}")
         order.status = "paid"
 class PaypalPaymentProcessor(PaymentProcessor):
-    def pay(self, order, security_code):
+    def __init__(self, email_address):
+        self.email_address = email_address
+    def pay(self, order):
         print("Processing credit payment type")
-        print(f"Verifying security code: {security_code}")
+        print(f"Verifying email address: {self.email_address}")
         order.status = "paid"
     
 
@@ -41,8 +47,8 @@ order.add_item("SSD", 1, 45)
 order.add_item("USB cable", 2, 50)
 
 print(order.total_price())
-processor = PaypalPaymentProcessor()
-processor.pay(order, "3247432")
+processor = PaypalPaymentProcessor('jhbdjfn@gmail.com')
+processor.pay(order)
 
 
 
